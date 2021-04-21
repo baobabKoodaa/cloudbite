4f0ab25eb659bcab8fb702f7209cfda6fecba26c
Atte Juvonen <git23123761293@baobab.fi>
2021-04-21 18:39:06 +0300

    Add support for multiple decks.

--- content/aws.js
+++ content/aws.js
@@ -1,4 +1,4 @@
-const flipcards = [
+const aws_cards = [
     {
         "q": "Differences and commonalities between pre-signed URLs and signed URLs?",
         "a": "Pre-signed URLs vs signed URLs<br><br><ul><li>Pre-signed URLs is a feature of S3.</li><li>Signed URLs is a feature of CloudFront.</li><li>Main use case for both is to share content privately with users, by providing a URL that includes a secret.</li><li>Both can be restricted with factors such as expiration date.</li><li>In addition, pre-signed URLs can be used to allow users to upload objects to buckets where they normally wouldn't have permissions to upload.</li></ul>"
